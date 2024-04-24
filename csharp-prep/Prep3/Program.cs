5ef685d8387d3ed89c11bc282983e4a40df3b311
Setsunani <145817758+Setsunani@users.noreply.github.com>
2024-04-24 08:40:18 -0300

    Update Program.cs
    prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("Welcome to Guess My Number!");
+
+        Console.Write("What is the magic number? ");
+        int magicNumber = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Try to guess the magic number!");
+
+        int guess;
+        int guessCount = 0;
+        do
+        {
+            Console.Write("What is your guess? ");
+            guess = Convert.ToInt32(Console.ReadLine());
+            guessCount++;
+
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        } while (guess != magicNumber);
+
+        Console.WriteLine($"You made {guessCount} guesses.");
+
+        Console.Write("Do you want to play again? (yes/no): ");
+        string playAgain = Console.ReadLine().ToLower();
+
+        if (playAgain == "yes")
+        {
+            Main(args);
+        }
+        else
+        {
+            Console.WriteLine("Thanks for playing!");
+        }
     }
 }
