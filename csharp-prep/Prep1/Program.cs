87da0095256b32d5bcbf45fbf94d1c634a58129e
Setsunani <145817758+Setsunani@users.noreply.github.com>
2024-04-24 08:23:45 -0300

    Update Program.cs
    prep1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,15 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Solicitar o primeiro nome do usuário
         Console.WriteLine("What is your first name?");
         string firstName = Console.ReadLine();
 
-        // Solicitar o último nome do usuário
         Console.WriteLine("What is your last name?");
         string lastName = Console.ReadLine();
 
-        // Exibir o nome completo no formato especificado
         Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
